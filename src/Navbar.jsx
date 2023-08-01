1ca424b199109045215744246a55c67d9299428f
Noah A <noah.ahmed.278@gmail.com>
2023-08-01 03:58:42 -0500

    navbar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return <nav className = "nav">
 
-    <Link to = "/"class = "site-title">Home</Link>
+    <Link to = "/Black/"class = "site-title">Home</Link>
     <ul>
         <li>
             <Link to='/about' >About</Link>
